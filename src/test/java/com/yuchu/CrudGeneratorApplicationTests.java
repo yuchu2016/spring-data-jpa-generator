0dc267dc97a72722a29cca52d89e707d91347d08
yuchu2016 <1804953654@qq.com>
2018-04-25 17:07:54 +0800

    spring data jpa crud code generator

--- src/test/java/com/yuchu/CrudGeneratorApplicationTests.java
+++ src/test/java/com/yuchu/CrudGeneratorApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RenrenApplicationTests {
+public class CrudGeneratorApplicationTests {
 
 	@Test
 	public void contextLoads() {
